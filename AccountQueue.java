e611360de11a7f01619fd1600a1fa2195d16f397
Alexander Topash <Alextopash@gmail.com>
2026-02-20 06:38:25 -0500

    enqueue functionality

--- AccountQueue.java
+++ AccountQueue.java
@@ -1,52 +1,70 @@
-public class AccountQueue
-{
+public class AccountQueue {
     private AccountNode qFront;
     private AccountNode qRear;
 
-    public AccountQueue()
-    {
+    public AccountQueue() {
         qFront = null;
-	qRear = null;
+        qRear = null;
     }
-        
-    public boolean isEmpty()
-    {
-        if(qFront==null) return true;
-	else return false;
+
+    public boolean isEmpty() {
+        if (qFront == null)
+            return true;
+        else
+            return false;
     }
-    
+
     public void enqueue(AccountNode n)
     {
-	// your code goes here
+	    // your code goes here
         // insert n to the back of queue
+        if(this.isEmpty()){
+            qFront = n;
+            qRear = n;
+        } else if (qFront == qRear){
+            AccountNode leader = qFront;
+            leader.next = n;
+            qRear = n;
+        } else {
+            AccountNode lead = qFront;
+            while(lead.next != null){
+                lead = lead.next;
+            }
+            lead.next = n;
+            qRear = n;
+        }
+
     }
-    
-    public void enqueue(int act_number, String name, double balance)
-    {
+
+    public void enqueue(int act_number, String name, double balance) {
         // your code goes here
         // create a new AccountNode instance n based on parameters
+        AccountNode newAccountNode = new AccountNode(act_number, name, balance);
         // call enqueue(n) to insert n to the back of queue;
+        this.enqueue(newAccountNode);
     }
-	
-    public AccountNode dequeue()
-    {
-	// your code goes here
+
+    public AccountNode dequeue() {
+        // your code goes here
         // if queue is not empty, remove and return one item from the front of queue
-	// else return null;
+        if(!this.isEmpty()){
+            AccountNode lead = qFront;
+            qFront.next = qFront;
+            return lead;
+        }
+        // else return null;
+        return null;
     }
-    
-    public void printQueue()
-    {
+
+    public void printQueue() {
         AccountNode temp = this.qFront;
-        while(temp != null)
-        {
+        while (temp != null) {
             System.out.println(temp.account_number + " " + temp.name + " " + temp.balance);
             temp = temp.next;
         }
     }
-    
-    public void reverseQueue()
-    {
+
+    public void reverseQueue() {
         // your code goes here
         // reverse the order of the elements in queue
     }
